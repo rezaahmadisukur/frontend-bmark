c97e2b1ae032dac850423d4e1a6c75b84551a1ad
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-06-01 16:55:32 +0700

    feat: add Topbar to dashboard layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { Sidebar } from "~/components/partials";
+import { Sidebar, Topbar } from "~/components/partials";
 
 const BookmarkDashboard = () => {
   return (
     <div className="flex h-screen overflow-hidden bg-background">
       {/* Sidebar */}
       <Sidebar />
+
+      {/* Main area */}
+      <div className="flex flex-1 flex-col overflow-hidden">
+        {/* Top bar */}
+        <Topbar />
+      </div>
     </div>
   );
 };
