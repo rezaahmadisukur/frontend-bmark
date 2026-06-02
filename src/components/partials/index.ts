f2e51bfb6fc2050c8deb2a249870e5621adb1b52
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-06-02 21:19:26 +0700

    feat(components): add TagFilter component to partials exports

--- src/components/partials/index.ts
+++ src/components/partials/index.ts
@@ -1,5 +1,6 @@
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import PageHeader from "./PageHeader";
+import TagFilter from "./TagFilter";
 
-export { Sidebar, Topbar, PageHeader };
+export { Sidebar, Topbar, PageHeader, TagFilter };
