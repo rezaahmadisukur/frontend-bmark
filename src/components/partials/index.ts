26c077978c335c5db3205796fbac7cec827b00a8
rezaahmadisukur <rezaahmadisukur1309@gmail.com>
2026-06-07 13:50:26 +0700

    feat: export new components MainContent, EmptyState, BookmarkCard

--- src/components/partials/index.ts
+++ src/components/partials/index.ts
@@ -2,5 +2,16 @@ import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 import PageHeader from "./PageHeader";
 import TagFilter from "./TagFilter";
+import MainContent from "./MainContent";
+import EmptyState from "./EmptyState";
+import BookmarkCard from "./BookmarkCard";
 
-export { Sidebar, Topbar, PageHeader, TagFilter };
+export {
+  Sidebar,
+  Topbar,
+  PageHeader,
+  TagFilter,
+  MainContent,
+  EmptyState,
+  BookmarkCard
+};
