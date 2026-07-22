d6d0e6733b782eb064859ce28af062b64c3ee297
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-07-22 19:35:29 +0700

    feat(auth): add LoginPage component with placeholder structure

---
+++ src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+// Validaton schema with zod
+// const LoginSchema =
+
+const LoginPage = () => {
+  return (
+    <>
+      <h1>Login Form ./components</h1>
+    </>
+  );
+};
+
+export default LoginPage;
