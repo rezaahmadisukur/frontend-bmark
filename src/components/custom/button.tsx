214e2ecc85e28b3916b955cf8cc1a9cf3c5514f2
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-07-26 16:00:19 +0700

    style(button): adjust shadow and active state for better press effect

--- src/components/custom/button.tsx
+++ src/components/custom/button.tsx
@@ -12,9 +12,8 @@ export function Button({ className, ...props }: ButtonProps) {
   return (
     <ShadcnButton
       className={cn(
-        "shadow-[0_6px_0_#4338ca]",
-        "hover:shadow-[0_4px_0_hsl(var(--primary))] hover:translate-y-0.5",
-        "active:shadow-[0_1px_0_hsl(var(--primary))] active:translate-y-1.25",
+        "shadow-[0_5px_0_#3730a3]",
+        "active:shadow-none active:translate-y-1.25",
         "transition-all duration-100",
         className
       )}
