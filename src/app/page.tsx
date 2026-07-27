162a2e58ec7796c8f7714be3b9e93befd39acbab
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-07-27 19:32:42 +0700

    refactor: replace BookmarkDashboard with placeholder Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,9 @@
-import {
-  AddBookmarkModal,
-  CommandPalette,
-  MainContent,
-  PageHeader,
-  Sidebar,
-  TagFilter,
-  Topbar
-} from "~/components/partials";
-
-const BookmarkDashboard = () => {
+const Home = () => {
   return (
-    <div className="flex h-screen overflow-hidden bg-background">
-      {/* Sidebar */}
-      <Sidebar />
-
-      {/* Main area */}
-      <div className="flex flex-1 flex-col overflow-hidden">
-        {/* Top bar */}
-        <Topbar />
-
-        {/* Content Area */}
-        <div className="flex flex-1 flex-col overflow-y-auto">
-          {/* Page header */}
-          <PageHeader />
-          {/* Tag filter bar */}
-          <TagFilter />
-          {/* Bookmark grid/list */}
-          <MainContent />
-        </div>
-      </div>
-
-      {/* Modals & Overlays */}
-      <AddBookmarkModal />
-      <CommandPalette />
+    <div>
+      <h1>HOME PAGE</h1>
     </div>
   );
 };
 
-export default BookmarkDashboard;
+export default Home;
