617b358f63c6e4b3839b0d1029f7c3c5ab3165f1
Reza Ahmadi Sukur <rezaahmadisukur1309@gmail.com>
2026-08-01 08:14:34 +0700

    refactor(index.ts): remove AddBookmarkModal import from partials

--- src/components/partials/index.ts
+++ src/components/partials/index.ts
@@ -4,7 +4,6 @@ import PageHeader from "./PageHeader";
 import TagFilter from "./TagFilter";
 import EmptyState from "../../features/bookmarks/components/EmptyState";
 import BookmarkCard from "../../features/bookmarks/components/BookmarkCard";
-import AddBookmarkModal from "./AddBookmarkModal";
 import CommandPalette from "./CommandPalette";
 
 export {
@@ -14,6 +13,5 @@ export {
   TagFilter,
   EmptyState,
   BookmarkCard,
-  AddBookmarkModal,
   CommandPalette
 };
